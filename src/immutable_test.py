ad393bb09f246d7d41e9416cbd4ef2eeb9bc8741
HLteam <2510555317@qq.com>
2020-05-07 15:05:56 +0800

    src

---
+++ src/immutable_test.py
@@ -0,0 +1,105 @@
+import unittest
+from hypothesis import given
+import hypothesis.strategies as st
+from immutable import *
+class TestImmutableList(unittest.TestCase):
+ def test_size(self):
+    lst = UnrolledLinkList()
+    self.assertEqual(lst.size(), 0)
+    lst.add(0, 'm')
+    self.assertEqual(lst.size(), 1)
+    lst.add(1, 'n')
+    self.assertEqual(lst.size(), 2)
+ def test_to_list(self):
+  self.assertEqual(UnrolledLinkList().to_list(), 0)
+  lst = UnrolledLinkList()
+  lst.add(0 , 'm')
+  self.assertEqual(lst.to_list(),['m'])
+  lst.add(1,'n')
+  self.assertEqual(lst.to_list(),['m', 'n'])
+
+def test_from_list(self):
+ test_data = [
+     [],
+     ['m'],
+     ['m', 'n']
+   ]
+ for e in test_data:
+    lst = UnrolledLinkList()
+    lst.from_list(e)
+    self.assertEqual(lst.to_list(), e)
+
+
+ def test_map(self):
+  lst = UnrolledLinkList()
+  lst.map(str)
+  self.assertEqual(lst.to_list(), [])
+  lst = UnrolledLinkList()
+  lst.from_list([1, 2, 3])
+  lst.map(str)
+  self.assertEqual(lst.to_list(), ["1", "2", "3"])
+
+def test_reduce(self):
+  lst = UnrolledLinkList()
+  self.assertEqual(lst.reduce(lambda st, e: st + e, 0), 0)
+ # sum of list
+  lst = UnrolledLinkList()
+  lst.from_list([1, 2, 3])
+  self.assertEqual(lst.reduce(lambda st, e: st + e, 0), 6)
+ # size
+  test_data = [
+     [],
+     ['m'],
+     ['m', 'n']
+  ]
+  for e in test_data:
+    lst = UnrolledLinkList()
+    lst.from_list(e)
+    self.assertEqual(lst.reduce(lambda st, _: st + 1, 0), lst.size())
+
+
+  @given(st.lists(st.integers()))
+  def test_from_list_to_list_equality(self, a):
+    lst = UnrolledLinkList()
+    lst.from_list(a)
+    b = lst.to_list()
+    self.assertEqual(a, b)
+  @given(st.lists(st.integers()))
+  def test_python_len_and_list_size_equality(self, a):
+      lst = UnrolledLinkList()
+      lst.from_list(a)
+      self.assertEqual(lst.size(), len(a))
+
+  def test_iter(self):
+     x = [1, 2, 3]
+     lst = UnrolledLinkList()
+     lst.from_list(x)
+     i = 0
+     tmp = []
+     for e in lst.root.items:
+         i += 1
+         if i> len(x):
+            break
+         tmp.append(e)
+     self.assertEqual(x, tmp)
+     self.assertEqual(lst.to_list(), tmp)
+
+  def find(self):
+     x = ['m' ,'n', 'o']
+     lst = UnrolledLinkList()
+     lst.from_list(x)
+     index = lst.find('n')
+     self.assertEqual(0,index)
+
+  def filter(self):
+     def f(x):
+        res = x+ 1
+        return res
+     x=[1,2,3]
+     lst=UnrolledLinkList()
+     lst.from_list(x)
+     lst.filter(f)
+     self.assertEqual([2,3,4],lst.to_list())
+
+  if __name__ == '__main__':
+     unittest.main()
---
+++ src/mutable.py
@@ -0,0 +1,135 @@
+class Node(object):
+    def __init__(self, max_capacity=16):
+        self.capacity = max_capacity
+        self.next = None
+        self.prev = None
+        self.items = [None]*max_capacity
+        self.value = None
+        self.size = 0
+
+
+
+class UnrolledLinkList(object):
+    def __init__(self):
+        self.sizeAll = 0
+        self.root = Node()
+
+
+    def __iter__(self):
+        return UnrolledLinkList()
+
+    def __next__(self):
+        if self.root is None:
+            raise StopIteration
+        while self.root is not None:
+            for i in range(0, self.root.size):
+                temp = self.root.items[i]
+                return temp
+            self.root = not self.root
+
+    def size(self):
+        return self.sizeAll
+
+    def add(self, index, e):
+        if index < 0 or index > self.sizeAll:
+            return
+        curNode = self.root
+        while index >= curNode.size:
+            if index == curNode.size:
+                break
+            index -= curNode.size
+            curNode = curNode.next
+
+        if curNode.size == curNode.capacity:
+            node = Node()
+            nextNode = curNode.next
+            curNode.next = node
+            node.next = nextNode
+
+            move_index = curNode.size // 2
+            for i in range(move_index, curNode.size):
+                node.items[i - move_index] = curNode.items[i]
+                curNode.items[i] = None
+                curNode -= 1
+                node.size += 1
+            if index >= move_index:
+                index -= move_index
+                curNode = node
+
+        for i in range(curNode.size - 1, index - 1, -1):
+            curNode.items[i + 1] = curNode.items[i]
+        curNode.items[index] = e
+        curNode.size += 1
+        self.sizeAll += 1
+
+    def remove(self, index):
+        if index < 0 or index > self.sizeAll:
+            return
+        curNode = self.root
+        while index >= curNode.size - 1:
+            if index == curNode.size - 1:
+                break
+            index -= curNode.size
+            curNode = curNode.next
+        for i in range(index, curNode - 1, 1):
+            curNode.items[i] = curNode.items[i + 1]
+        curNode.items[curNode.size - 1] = None
+        curNode.size -= 1
+
+        if curNode.capacity >= curNode.size + curNode.next.size and curNode.next.cap != -1:
+            nextNode = curNode.next
+            for i in range(0, nextNode.size):
+                curNode.items[curNode.size + 1] = nextNode.items[i]
+            curNode.size += nextNode.size
+            curNode.next = not nextNode.next
+        self.sizeAll -= 1
+
+    def to_list(self):
+        res = []
+        curNode = self.root
+        while curNode is not None:
+            for i in range(0, curNode.size):
+                res.append(curNode.items[i])
+            curNode = curNode.next
+        return res
+
+    def from_list(self, lst):
+        if len(lst) == 0:
+            self.root = None
+            return
+        curNode = self.root
+        for e in reversed(lst):
+            self.add(0, e)
+
+    def find(self, data):
+        curNode = self.root
+        count = 0
+        while curNode is not None:
+            for i in range(0, curNode.size):
+                if data == curNode.items[i]:
+                    count += 1
+                    index = count - 1
+                    return index
+            return -1
+
+    def filter(self, f):
+        curNode = self.root
+        for i in range(0, curNode.size):
+            curNode.items[i] = f(curNode.items[i])
+        return self.to_list()
+
+    def map(self, f):
+        curNode = self.root
+        while curNode is not None:
+            for i in range(0, curNode.size):
+                curNode.items[i] = f(curNode.items[i])
+            curNode = curNode.next
+
+    def reduce(self, f, initial_state):
+        state = initial_state
+        curNode = self.root
+        while curNode is not None:
+            for i in range(0, curNode.size):
+                state = f(state, curNode.items[i])
+            curNode = curNode.next
+        return state
